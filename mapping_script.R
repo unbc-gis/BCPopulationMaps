c4c94e0169b53d8e57239ea7e47d46865c18ae12
Geoguy <matt@geoguy.ca>
2022-04-27 18:19:34 -0700

    First Draft

--- mapping_script.R
+++ mapping_script.R
@@ -6,11 +6,14 @@ library(sf)
 low_pct = -0.4
 high_pct = 0.4
 
+lim = 1 #Distance for 0 where colour is maximum saturation
+
 low_colour = 'red'
 mid_colour = 'white'
 high_colour = 'blue'
 
-
+low_label = paste0("< ", toString(low_pct), "%")
+high_label = paste0("> ", toString(high_pct), "%")
 
 BCPopulation <- read_csv("BCPopulation1971-2021.csv") # Import Population From CSV
 years = names(BCPopulation)[3:length(names(BCPopulation))] # Ignore Names from CSV
@@ -33,41 +36,42 @@ for (i in length(years):2){
     pop_polygons[paste0("pct_", diff_name)] = pct
 
     pop_polygons[paste0("colour_", diff_name)] = 
-      ifelse(pct <= low_pct, 'red', 
-             ifelse(pct < high_pct, 'white', 'blue'))
+      ifelse(pct <= low_pct, 'low', 
+             ifelse(pct < high_pct, 'mid', 'high'))
   }
 }
 
-i = '1976'
-j = '2021'
-
-target_column = paste0("pct_", i, "_", j)
-map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = get(target_column))) +
-    scale_fill_binned(breaks = c(-0.4, 0.1, 0.2, 0.4),
-                      show.limits = TRUE) +
-  guides(fill=guide_legend(title=paste0("British Columbia\n%pop. change ", i, "-", j)))
-
-map + theme_void() + theme(legend.background = element_rect(linetype = 1, size = 0.25, colour = 1), 
+for (i in length(years):2){
+  for (j in (i-1):1){
+    pct_column = paste0("pct_", years[j], "_", years[i])
+    colour_column = paste0("colour_", years[j], "_", years[i])
+    
+    gradiant_out = paste0("output/gradient/", pct_column, ".png")
+    
+    pct_map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = get(pct_column))) +
+      scale_fill_gradient2(low = low_colour, mid = mid_colour, midpoint = 0,
+                           high = high_colour, limits = c(-lim, lim), n.breaks = 3,
+                           oob = scales::squish, labels = c(paste0("< -", lim, "%"), "0%", paste0("> ", lim, "%"))) +
+      guides(fill=guide_colorbar(title=paste0("British Columbia\n%pop. change ", years[i], "-", years[j]))) +
+      theme_void() + theme(legend.background = element_rect(linetype = 1, size = 0.25, colour = 1), 
                            legend.margin = margin(.25, 0.25, 0.25, 0.25, unit='cm'),
-                           legend.box.margin = margin(-7, 0, 0, -4, unit='cm'),
+                           legend.box.margin = margin(-7.8, 0, 0, -5.6, unit='cm'),
                            legend.title.align = 0.5)
-
-map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = get(target_column))) +
-  scale_fill_gradient2(  limits = c(-1, 1),
-                        low = low_colour,
-                        mid = mid_colour,
-                        midpoint = 0,
-                        high = high_colour,
-                        guide = "colourbar",
-                        aesthetics = "fill") +
-  guides(fill=guide_legend(title=paste0("British Columbia\n%pop. change ", i, "-", j)))
-
-map + theme_void() + theme(legend.background = element_rect(linetype = 1, size = 0.25, colour = 1), 
+    pct_map
+    ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = gradiant_out)
+    
+    colour_column = paste0("colour_", years[j], "_", years[i])
+    classified_out = paste0("output/classified/", colour_column, ".png")
+    
+    colour_map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = factor(get(colour_column)))) +
+      scale_fill_manual(na.value = "grey50", values = c(low = low_colour, mid = mid_colour, high = high_colour),
+                        labels = c(low_label, "", high_label)) +
+      guides(fill=guide_legend(title=paste0("British Columbia\n%pop. change ", years[i], "-", years[j]), reverse = TRUE)) +
+      theme_void() + theme(legend.background = element_rect(linetype = 1, size = 0.25, colour = 1), 
                            legend.margin = margin(.25, 0.25, 0.25, 0.25, unit='cm'),
-                           legend.box.margin = margin(-7, 0, 0, -4, unit='cm'),
+                           legend.box.margin = margin(-7.8, 0, 0, -5.6, unit='cm'),
                            legend.title.align = 0.5)
-
-                           
-
-                           
-                           
+    
+    ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = classified_out)
+  }
+}
