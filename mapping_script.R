df5ef57db64dfa2aa902e051af75d0623b6b7c4a
Geoguy <matt@geoguy.ca>
2022-04-27 18:41:48 -0700

    Fix output file names

--- mapping_script.R
+++ mapping_script.R
@@ -46,32 +46,35 @@ for (i in length(years):2){
     pct_column = paste0("pct_", years[j], "_", years[i])
     colour_column = paste0("colour_", years[j], "_", years[i])
     
-    gradiant_out = paste0("output/gradient/", pct_column, ".png")
+    gradiant_out = paste0("output/gradient/", pct_column)
     
     pct_map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = get(pct_column))) +
       scale_fill_gradient2(low = low_colour, mid = mid_colour, midpoint = 0,
                            high = high_colour, limits = c(-lim, lim), n.breaks = 3,
                            oob = scales::squish, labels = c(paste0("< -", lim, "%"), "0%", paste0("> ", lim, "%"))) +
-      guides(fill=guide_colorbar(title=paste0("British Columbia\n%pop. change ", years[i], "-", years[j]))) +
+      guides(fill=guide_colorbar(title=paste0("British Columbia\n%pop. change ", years[j], "-", years[i]))) +
       theme_void() + theme(legend.background = element_rect(linetype = 1, size = 0.25, colour = 1), 
                            legend.margin = margin(.25, 0.25, 0.25, 0.25, unit='cm'),
                            legend.box.margin = margin(-7.8, 0, 0, -5.6, unit='cm'),
                            legend.title.align = 0.5)
     pct_map
-    ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = gradiant_out)
+    ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = paste0(gradiant_out, ".jpg"))
+    ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = paste0(gradiant_out, ".png"))
     
     colour_column = paste0("colour_", years[j], "_", years[i])
-    classified_out = paste0("output/classified/", colour_column, ".png")
+    classified_out = paste0("output/classified/", colour_column)
     
     colour_map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = factor(get(colour_column)))) +
       scale_fill_manual(na.value = "grey50", values = c(low = low_colour, mid = mid_colour, high = high_colour),
                         labels = c(low_label, "", high_label)) +
-      guides(fill=guide_legend(title=paste0("British Columbia\n%pop. change ", years[i], "-", years[j]), reverse = TRUE)) +
+      guides(fill=guide_legend(title=paste0("British Columbia\n%pop. change ", years[j], "-", years[i]), reverse = TRUE)) +
       theme_void() + theme(legend.background = element_rect(linetype = 1, size = 0.25, colour = 1), 
                            legend.margin = margin(.25, 0.25, 0.25, 0.25, unit='cm'),
                            legend.box.margin = margin(-7.8, 0, 0, -5.6, unit='cm'),
                            legend.title.align = 0.5)
     
-    ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = classified_out)
+    ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = paste0(classified_out, ".jpg"))
+    ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = paste0(classified_out, ".png"))
   }
 }
+
