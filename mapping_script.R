7c68904cb9d89cc61abc85afca72ecbb14df8328
Geoguy <matt@geoguy.ca>
2022-04-27 21:22:26 -0700

    Fix missing boundaries in categorical plots

--- mapping_script.R
+++ mapping_script.R
@@ -21,32 +21,29 @@ years = names(BCPopulation)[3:length(names(BCPopulation))] # Ignore Names from C
 
 boundaries <- st_read("basemap/REGIONAL_DISTRICTS.geojson") # Open Regional Districts
 
-pop_polygons <- merge(boundaries, BCPopulation) # Add pop data to polygons
-
-
-st_crs(pop_polygons) # Test that data has projection
-
 for (i in length(years):2) {
   for (j in (i-1):1) {
     diff_name = paste0(years[j], "_", years[i])
     print(diff_name)
-    pop_polygons[paste0("change_", diff_name)] = 
+    BCPopulation[paste0("change_", diff_name)] = 
       BCPopulation[years[i]] - BCPopulation[years[j]]
     pct = ((1 / BCPopulation[years[j]]) * BCPopulation[years[i]]) - 1
-    pop_polygons[paste0("pct_", diff_name)] = pct
+    BCPopulation[paste0("pct_", diff_name)] = pct
 
-    pop_polygons[paste0("colour_", diff_name)] = 
+    BCPopulation[paste0("colour_", diff_name)] = 
       ifelse(pct <= low_pct, 'low', 
              ifelse(pct < high_pct, 'mid', 'high'))
   }
 }
 
+pop_polygons <- merge(boundaries, BCPopulation) # Add pop data to polygons
+
 for (i in length(years):2) {
   for (j in (i-1):1) {
     pct_column = paste0("pct_", years[j], "_", years[i])
     colour_column = paste0("colour_", years[j], "_", years[i])
-    
-    gradiant_out = paste0("output/gradient/", pct_column)
+    print(pct_column)
+    gradiant_out = paste0("output/", pct_column)
     
     pct_map <- ggplot() + geom_sf(data = pop_polygons, color = 'black', aes(fill = get(pct_column))) +
       scale_fill_gradient2(low = low_colour, mid = mid_colour, midpoint = 0,
@@ -62,10 +59,10 @@ for (i in length(years):2) {
     ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = paste0(gradiant_out, ".png"))
     
     colour_column = paste0("colour_", years[j], "_", years[i])
-    classified_out = paste0("output/classified/", colour_column)
+    classified_out = paste0("output/", colour_column)
     
-    colour_map <- ggplot() + geom_sf(data = pop_polygons, color = 'black', aes(fill = factor(get(colour_column)))) +
-      scale_fill_manual(na.value = "grey50", values = c(low = low_colour, mid = mid_colour, high = high_colour),
+    colour_map <- ggplot() + geom_sf(data = pop_polygons, color = 'black', aes(fill = get(colour_column))) +
+      scale_fill_manual(na.value = "grey50", values = c('low' = low_colour, 'mid' = mid_colour, 'high' = high_colour),
                         labels = c(low_label, "", high_label)) +
       guides(fill=guide_legend(title=paste0("British Columbia\n%pop. change ", years[j], "-", years[i]), reverse = TRUE)) +
       theme_void() + theme(legend.background = element_rect(linetype = 1, size = 0.25, colour = 1), 
@@ -73,8 +70,8 @@ for (i in length(years):2) {
                            legend.box.margin = margin(-7.8, 0, 0, -5.6, unit='cm'),
                            legend.title.align = 0.5)
     
+    colour_map = colour_map + geom_sf(data = pop_polygons, colour = 'black', fill = NA, show.legend =  FALSE)
     #ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = paste0(classified_out, ".jpg"))
     ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = paste0(classified_out, ".png"))
   }
 }
-
