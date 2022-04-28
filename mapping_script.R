b183faefe8c1b13eba680bff787697ce5bade57d
Geoguy <matt@geoguy.ca>
2022-04-27 20:00:20 -0700

    Fix boarders

--- mapping_script.R
+++ mapping_script.R
@@ -26,8 +26,8 @@ pop_polygons <- merge(boundaries, BCPopulation) # Add pop data to polygons
 
 st_crs(pop_polygons) # Test that data has projection
 
-for (i in length(years):2){
-  for (j in (i-1):1){
+for (i in length(years):2) {
+  for (j in (i-1):1) {
     diff_name = paste0(years[j], "_", years[i])
     print(diff_name)
     pop_polygons[paste0("change_", diff_name)] = 
@@ -41,14 +41,14 @@ for (i in length(years):2){
   }
 }
 
-for (i in length(years):2){
-  for (j in (i-1):1){
+for (i in length(years):2) {
+  for (j in (i-1):1) {
     pct_column = paste0("pct_", years[j], "_", years[i])
     colour_column = paste0("colour_", years[j], "_", years[i])
     
     gradiant_out = paste0("output/gradient/", pct_column)
     
-    pct_map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = get(pct_column))) +
+    pct_map <- ggplot() + geom_sf(data = pop_polygons, color = 'black', aes(fill = get(pct_column))) +
       scale_fill_gradient2(low = low_colour, mid = mid_colour, midpoint = 0,
                            high = high_colour, limits = c(-lim, lim), n.breaks = 3,
                            oob = scales::squish, labels = c(paste0("< -", lim, "%"), "0%", paste0("> ", lim, "%"))) +
@@ -57,14 +57,14 @@ for (i in length(years):2){
                            legend.margin = margin(.25, 0.25, 0.25, 0.25, unit='cm'),
                            legend.box.margin = margin(-7.8, 0, 0, -5.6, unit='cm'),
                            legend.title.align = 0.5)
-    pct_map
-    ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = paste0(gradiant_out, ".jpg"))
+    
+    #ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = paste0(gradiant_out, ".jpg"))
     ggsave(plot = pct_map, width = 7, height = 6, dpi = 1200, filename = paste0(gradiant_out, ".png"))
     
     colour_column = paste0("colour_", years[j], "_", years[i])
     classified_out = paste0("output/classified/", colour_column)
     
-    colour_map <- ggplot() + geom_sf(data = pop_polygons, aes(fill = factor(get(colour_column)))) +
+    colour_map <- ggplot() + geom_sf(data = pop_polygons, color = 'black', aes(fill = factor(get(colour_column)))) +
       scale_fill_manual(na.value = "grey50", values = c(low = low_colour, mid = mid_colour, high = high_colour),
                         labels = c(low_label, "", high_label)) +
       guides(fill=guide_legend(title=paste0("British Columbia\n%pop. change ", years[j], "-", years[i]), reverse = TRUE)) +
@@ -73,7 +73,7 @@ for (i in length(years):2){
                            legend.box.margin = margin(-7.8, 0, 0, -5.6, unit='cm'),
                            legend.title.align = 0.5)
     
-    ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = paste0(classified_out, ".jpg"))
+    #ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = paste0(classified_out, ".jpg"))
     ggsave(plot = colour_map, width = 7, height = 6, dpi = 1200, filename = paste0(classified_out, ".png"))
   }
 }
